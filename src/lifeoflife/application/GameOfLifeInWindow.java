6762fbd0111b0463abde8d5659d24a9afeb97c2e
Sergej Povzaniuk <liravanigroup@gmail.com>
2016-04-28 23:56:40 +0200

    Added files via upload

--- src/lifeoflife/application/GameOfLifeInWindow.java
+++ src/lifeoflife/application/GameOfLifeInWindow.java
@@ -3,14 +3,18 @@ package lifeoflife.application;
  * Sergej Povzaniuk
  * 2016-04-27.
  */
-import lifeoflife.model.GameEngine;
-import java.io.IOException;
 
-public class GameOfLifeInWindow  {
-    public static void main(String[] args) throws InterruptedException, IOException {
-        GameEngine game = new GameEngine(100, 100, 0.10, 5);
-        game.setUpdateSpeed(100);
-        game.start();
+import lifeoflife.model.GameEngine;
+
+public class GameOfLifeInWindow {
+    public static void main(String[] args) {
+        try {
+            GameEngine game = new GameEngine(300, 150, 0.25, 5);
+            game.setUpdateSpeed(100);
+            game.start();
+        } catch (InterruptedException | IllegalArgumentException ex) {
+            System.out.println(ex.getMessage());
+        }
     }
 }
 
